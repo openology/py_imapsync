6ac1cb626750ed5e7653aaa793216c6a8cfa7be9
Randy Sage <r@thinhorn.com>
2015-06-20 17:34:21 +0000

    1. Update move_trash_to_archive.py for improved output control 2. Update move_trash_to_archive.py to use credentials.py (if available)

--- move_trash_to_archive.py
+++ move_trash_to_archive.py
@@ -1,20 +1,30 @@
 #! /usr/bin/env python
 
-import email, getpass, imaplib, os, re
+import email, getpass, imaplib, os, re, time
+
+results_string = ''
 
 pattern_uid = re.compile('\d+ \(UID (?P<uid>\d+)\)')
 
 def parse_uid(data):
-    match = pattern_uid.match(data)
-    return match.group('uid')
+    match = pattern_uid.match(str(data))
+    if match:
+        return match.group('uid')
+    else:
+        return match
 
 detach_dir = '.' # directory where to save attachments (default: current)
-user = raw_input("Enter your GMail username:")
-pwd = getpass.getpass("Enter your password: ")
+
+try:
+    from credentials import user, password, from_address
+except:
+    user = raw_input("Enter your GMail username:")
+    password = getpass.getpass("Enter your password: ")
+    from_address = "me@example.com"
 
 # connecting to the gmail imap server
 m = imaplib.IMAP4_SSL("imap.gmail.com")
-m.login(user,pwd)
+m.login(user,password)
 #print m.list()
 m.select("[Gmail]/Trash") # here you a can choose a mail box like INBOX instead
 # use m.list() to get all the mailboxes
@@ -23,27 +33,38 @@ resp, items = m.search(None, "ALL") # you could filter using the IMAP rules here
 items = items[0].split() # getting the mails id
 
 if len(items):
-    print '------------About to move messages'
-    
-    for emailid in items:
-        # First get UID
-        resp, data = m.fetch(emailid, "(UID)")
-        msg_uid = parse_uid(data[0])    
+    try:
+        results_string += '------------About to move messages\n'
         
-        # Log email summary data
-        resp, data = m.fetch(emailid, "(RFC822)") # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
-        email_body = data[0][1] # getting the mail content
-        mail = email.message_from_string(email_body) # parsing the mail content to get a mail object
-        
-        print "["+mail["From"]+"] :" + mail["Subject"] , 
-        
-        result = m.uid('COPY', msg_uid, 'zzzz_toArchive')
-        if result[0] == 'OK':
-            mov, data = m.uid('STORE', msg_uid , '+FLAGS', '(\Deleted)')
-            m.expunge()
-            print " - SUCCESS"
-        else:
-            print " - FAIL"
-
-
-    print '------------Done'
+        for emailid in items:
+            # First get UID
+            resp, data = m.fetch(emailid, "(UID)")
+            msg_uid = parse_uid(data[0])    
+            
+            if msg_uid:
+                # Log email summary data
+                resp, data = m.fetch(emailid, "(RFC822)") # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
+                email_body = data[0][1] # getting the mail content
+                mail = email.message_from_string(email_body) # parsing the mail content to get a mail object
+ 
+               
+                results_string += "["+str(mail["From"])+"] :" + str(mail["Subject"])
+                
+                result = m.uid('COPY', msg_uid, 'zzzz_toArchive')
+                if result[0] == 'OK':
+                    mov, data = m.uid('STORE', msg_uid , '+FLAGS', '(\Deleted)')
+                    m.expunge()
+                    results_string += " - SUCCESS\n"
+                else:
+                    results_string += " - FAIL\n"
+            else:
+                results_string += "UNABLE TO MOVE/COPY... ["+str(mail["From"])+"] :" + str(mail["Subject"])
+        results_string += '------------Done\n'
+        print results_string
+        new_message = email.message.Message()
+        new_message["From"] = from_address
+        new_message["Subject"] = "Messages moved from trash to archive"
+        new_message.set_payload(results_string)
+        m.append('INBOX', '', imaplib.Time2Internaldate(time.time()), str(new_message))
+    except ValueError:
+        print 'ERROR OF SOME SORT'
